437b7e7116d9debe5ea789b66c70d4f5a346d44d
RasoolBux123 <rbmalik25@gmail.com>
2024-10-29 16:15:35 +0500

    Initial commit

--- task-project/src/App.jsx
+++ task-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
-import scrollicon from './components/scrollicon';
+import Scrollicon from './components/Scrollicon';
 import MobilestockSection from './components/MobilestackSection';
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       
       <Navbar />
       <Hero />
-      <scrollicon />
+      <Scrollicon />
       <MobilestockSection />
     </div>
   )
